4903bd70ae081d40fed3e78d799c48d99d980820
fantasibarani <sibarani13>
2019-05-01 07:47:21 +0700

    Add project files.

---
+++ Dana_Desa_App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dana_Desa_App.MvcApplication" Language="C#" %>
---
+++ Dana_Desa_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dana_Desa_App.Startup))]
+namespace Dana_Desa_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
